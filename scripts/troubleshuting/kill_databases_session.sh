fb629801ea3972885a43f7bbf2671583004c5f0c
魏亚萍 <weiyaping@jiagouyun.com>
2018-10-25 14:18:24 +0800

    1025

--- scripts/troubleshuting/kill_databases_session.sh
+++ scripts/troubleshuting/kill_databases_session.sh
@@ -6,6 +6,6 @@ host=localhost
 port=3306
 
 
-mysql -u$user -p$password -h$host  -P$port -e "select concat('kill ',id,';') from information_schema.processlist where table_schema in ("dbname","dbname2") > tmpfile
+mysql -u$user -p$password -h$host  -P$port -e "select concat('kill ',id,';') from information_schema.processlist where db in ("dbname","dbname2") > tmpfile
 
 awk '{if (NR != 1) print $0 }' tmpfile | mysql -u$user -p$password -h$host  -P$port 
